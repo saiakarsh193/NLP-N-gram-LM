856977eea9d8f128f4d35b245c82dd40bf1a9524
saiakarsh193 <sai.akarsh@research.iiit.ac.in>
2022-02-05 22:40:24 +0530

    Added laplace smoothing

--- language_model.py
+++ language_model.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import math
 from tokenizer import tokenize
 
 class LanguageModel():
@@ -7,8 +8,8 @@ class LanguageModel():
         self.ngram = ngram
         self.smooth = self.checkSmoothType(smooth)
         self.rawdata = self.readFile(path)
-        self.train_data, self.test_data = self.splitTrainTestandTokenize(5, 0)
-        self.vocabulary = self.makeVocabulary()
+        self.train_data, self.test_data = self.splitTrainTestandTokenize(1000, -1)
+        self.vocab, self.vocab_length = self.makeVocabulary()
         self.train()
 
     def checkSmoothType(self, smooth):
@@ -35,18 +36,31 @@ class LanguageModel():
         if(random_seed >= 0):
             random.seed(random_seed)
         random.shuffle(self.rawdata)
-        return self.tokenizeDataset(self.rawdata[test_count:]), self.tokenizeDataset(self.rawdata[0:test_count])
+        return self.tokenizeDataset(self.rawdata[test_count:]), self.rawdata[0:test_count]
 
     def makeVocabulary(self):
         vocab = {}
-        # for tokens in self.train_data:
-        #     for token in tokens:
-        #         if 
-        return vocab
+        for tokens in self.train_data:
+            for token in tokens:
+                if token in vocab:
+                    vocab[token] += 1
+                else:
+                    vocab[token] = 1
+        uk_cnt = 0
+        for key, val in list(vocab.items()):
+            if(val == 1):
+                del vocab[key]
+                uk_cnt += 1
+        vocab['<UKN>'] = uk_cnt
+        return vocab, len(vocab.keys())
+
+    def updateUnkSentence(self, tokens):
+        return [(token if token in self.vocab else '<UKN>') for token in tokens]
 
     def train(self):
         tmodel = {}
         for tokens in self.train_data:
+            tokens = self.updateUnkSentence(tokens)
             temp = (['<START>'] * (self.ngram - 1)) + tokens
             for i in range(self.ngram - 1, len(temp)):
                 base = ' '.join(temp[i - (self.ngram - 1): i])
@@ -60,24 +74,45 @@ class LanguageModel():
                     tmodel[base]["<TOTAL_COUNT>"] += 1
         self.tmodel = tmodel
 
+    def getProb(self, word, base):
+        if(base in self.tmodel):
+            cbase = self.tmodel[base]["<TOTAL_COUNT>"]
+        else:
+            cbase = 0
+        if(word != "<TOTAL_COUNT>" and cbase > 0 and word in self.tmodel[base]):
+            ctotal = self.tmodel[base][word]
+        else:
+            ctotal = 0
+        if(self.smooth == 'l'):
+            pw = (1 + ctotal) / (self.vocab_length + cbase)
+        else:
+            pw = 1
+        return pw
+
     def getPerplexity(self, sentence):
-        temp = tokenize(sentence.strip())
+        temp = self.updateUnkSentence(tokenize(sentence.strip()))
+        n = len(temp)
         temp = (['<START>'] * (self.ngram - 1)) + temp
+        ppw = 1
         for i in range(self.ngram - 1, len(temp)):
             base = ' '.join(temp[i - (self.ngram - 1): i])
             word = temp[i]
-            if(base in self.tmodel):
-                cbase = self.tmodel[base]["<TOTAL_COUNT>"]
-            else:
-                cbase = 0
-            if(word != "<TOTAL_COUNT>" and cbase > 0 and word in self.tmodel[base]):
-                ctotal = self.tmodel[base][word]
-            else:
-                ctotal = 0
-            # pw = 
-        return
+            ppw *= self.getProb(word, base)
+        try:
+            ppw = math.pow(ppw, -1/n)
+        except Exception as exception:
+            ppw = 1
+        return ppw
+
+    def test(self):
+        tsum = 0
+        for sentence in self.test_data:
+            # print(sentence, self.getPerplexity(sentence))
+            tsum += self.getPerplexity(sentence)
+        print(tsum / len(self.test_data))
 
 if(len(sys.argv) == 4):
     lm = LanguageModel(int(sys.argv[1]), sys.argv[2], sys.argv[3])
+    lm.test()
 else:
     print("Invalid number of parameters")
